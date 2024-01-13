af905c7c64c6f9c34c1ed3d2b39929c2e55cbe43
uncom1c <mishartem2004@gmail.com>
2024-01-14 01:35:58 +0300

    123

--- task-1/service/venv/Scripts/activate.bat
+++ task-1/service/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\repo\grishanigger\task-1\service\venv
+set VIRTUAL_ENV=C:\Users\USER\Documents\GitHub\grishachert1\task-1\service\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
